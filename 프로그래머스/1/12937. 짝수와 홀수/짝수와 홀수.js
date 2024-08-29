a19ca27b034ff5f9d4f4fc4e3d3af7b1fff02c7c
김수빈 <rlatnqls311@gmail.com>
2024-08-29 12:39:50 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -0,0 +1,9 @@
+function solution(num) {
+    let answer = '';
+        if (num % 2 === 0) //정수를 2로 나눈 나머지가 0일 경우 짝수이고, 0이 아닐 경우 홀수이다.
+            return answer = "Even" // 나머지가 0이라면 "Even"
+        else {
+            return answer = "Odd" // 그게 아니라면 "Odd"
+        }
+    return answer;
+}
